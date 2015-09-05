0c7daa889771346d78893c5a2891c4e0ffc611cf
Ryan Betker <technologyRyan@outlook.com>
2015-09-05 11:22:08 -0500

    Adding default Bike controller

---
+++ BikeStore/Controllers/Admin/BikeController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using BikeStore;
+using BikeStore.Models;
+
+namespace BikeStore.Controllers.Admin
+{
+    public class BikeController : Controller
+    {
+        private BikeStoreContext db = new BikeStoreContext();
+
+        // GET: Bike
+        public ActionResult Index()
+        {
+            return View(db.Bikes.ToList());
+        }
+
+        // GET: Bike/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Bike bike = db.Bikes.Find(id);
+            if (bike == null)
+            {
+                return HttpNotFound();
+            }
+            return View(bike);
+        }
+
+        // GET: Bike/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Bike/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "BikeID,ModelNo,Type,FrameSize,WheelSize,Color,BrandName,CreatedBy,CreatedDate,ModifiedBy,ModifiedDate")] Bike bike)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Bikes.Add(bike);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(bike);
+        }
+
+        // GET: Bike/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Bike bike = db.Bikes.Find(id);
+            if (bike == null)
+            {
+                return HttpNotFound();
+            }
+            return View(bike);
+        }
+
+        // POST: Bike/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "BikeID,ModelNo,Type,FrameSize,WheelSize,Color,BrandName,CreatedBy,CreatedDate,ModifiedBy,ModifiedDate")] Bike bike)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(bike).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(bike);
+        }
+
+        // GET: Bike/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Bike bike = db.Bikes.Find(id);
+            if (bike == null)
+            {
+                return HttpNotFound();
+            }
+            return View(bike);
+        }
+
+        // POST: Bike/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Bike bike = db.Bikes.Find(id);
+            db.Bikes.Remove(bike);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
