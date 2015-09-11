a03fb0eba2e61fc9f6d661049a77493661815f19
Ryan Betker <technologyRyan@outlook.com>
2015-09-10 22:07:28 -0500

    Brand CRUD screens and button link to them started

--- BikeStore/Controllers/Admin/BrandController.cs
+++ BikeStore/Controllers/Admin/BrandController.cs
@@ -1,14 +1,15 @@
 ﻿using BikeStore.ViewModels;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
 namespace BikeStore.Controllers.Admin
 {
-    [RoutePrefix("Admin")]
-    [Route("action=index")]
+    [RoutePrefix("Admin/Brand")]
+    //[Route("action=index")]
     public class BrandController : Controller
     {
         private BikeStoreCustomContext db = new BikeStoreCustomContext();
@@ -16,7 +17,10 @@ namespace BikeStore.Controllers.Admin
         // GET: Brand
         public ActionResult Index()
         {
-            return View();
+            var brandList = db.Brands.ToList();
+            var brandViewModels = AutoMapper.Mapper.Map<IList<BrandViewModel>>(brandList);
+
+            return View(brandViewModels);
         }
 
         // GET: Brand/Details/5
@@ -33,21 +37,27 @@ namespace BikeStore.Controllers.Admin
 
         // POST: Brand/Create
         [HttpPost]
-        public ActionResult Create(BrandViewModel brandViewModel)
+        public ActionResult Create([Bind(Include="BrandID, BrandName")] BrandViewModel brandViewModel)
         {
-            try
+            if (ModelState.IsValid)
             {
-                brandViewModel.CreatedBy = this.GetUserName();
-                brandViewModel.CreatedDate = DateTime.Now;
+                try
+                {
+                    brandViewModel.CreatedBy = this.GetUserName();
+                    brandViewModel.CreatedDate = DateTime.Now;
 
-                var brand = AutoMapper.Mapper.Map<BikeStore.Models.Brand>(brandViewModel);
-                db.Brands.Add(brand);
-                return RedirectToAction("Index");
+                    var brand = AutoMapper.Mapper.Map<BikeStore.Models.Brand>(brandViewModel);
+                    db.Brands.Add(brand);
+                    return RedirectToAction("Index");
+                }
+                catch (Exception ex)
+                {
+                    //TODO: Add Elmah error logging to log the ex.
+                    ModelState.AddModelError("BrandID", "Error creating the brand.");
+                    return View();
+                }
             }
-            catch
-            {
-                return View();
-            }
+            return View();
         }
 
         // GET: Brand/Edit/5
@@ -58,17 +68,46 @@ namespace BikeStore.Controllers.Admin
 
         // POST: Brand/Edit/5
         [HttpPost]
-        public ActionResult Edit(int id, FormCollection collection)
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include="BrandID, BrandName")] BrandViewModel brand)
         {
-            try
+            if (ModelState.IsValid)
             {
-                // TODO: Add update logic here
+                try
+                {
+                    //Assign audit columns
+                    brand.ModifiedBy = this.GetUserName();
+                    brand.ModifiedDate = DateTime.Now;
 
-                return RedirectToAction("Index");
+                    var brandEntityToUpdate = db.Brands.First(b => b.BrandID == brand.BrandID);
+
+                    //ensure these values get set to some values as they're Required and EF will freak if not given.
+                    //I didn't allow them to be Bound because the user could have changed
+                    brand.CreatedBy = brandEntityToUpdate.CreatedBy;
+                    brand.CreatedDate = brandEntityToUpdate.CreatedDate;
+
+                    brandEntityToUpdate = AutoMapper.Mapper.Map(brand, brandEntityToUpdate);
+
+                    db.Entry(brandEntityToUpdate).State = EntityState.Modified;
+
+                    //these audit columns' values shouldn't change at all or it wrecks the history data:
+                    db.Entry(brandEntityToUpdate).Property(b => b.CreatedBy).IsModified = false;
+                    db.Entry(brandEntityToUpdate).Property(b => b.CreatedDate).IsModified = false;
+
+                    db.SaveChanges();
+
+                    return RedirectToAction("Index");
+                }
+                catch(Exception ex)
+                {
+                    //TODO: Add Elmah error logging to log the ex.
+                    ModelState.AddModelError("BrandID", "Error updating the brand.");
+                    return View();
+                }
             }
-            catch
+            else
             {
-                return View();
+                return View(brand);
             }
         }
 
@@ -79,17 +118,21 @@ namespace BikeStore.Controllers.Admin
         }
 
         // POST: Brand/Delete/5
-        [HttpPost]
-        public ActionResult Delete(int id, FormCollection collection)
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
         {
             try
             {
-                // TODO: Add delete logic here
+                var brand = db.Brands.Find(id);
+                db.Brands.Remove(brand);
+                db.SaveChanges();
 
                 return RedirectToAction("Index");
             }
-            catch
+            catch (Exception ex)
             {
+                //TODO: Add Elmah error logging to log the ex.
+                ModelState.AddModelError("BrandID", "Error deleting the brand.");
                 return View();
             }
         }
