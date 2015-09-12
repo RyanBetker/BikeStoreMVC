429ae60e54be8355d72ca3102c741fe9f489180b
Ryan Betker <technologyRyan@outlook.com>
2015-09-11 23:30:27 -0500

    Adding Brand delete and details, as well as a JS

---
+++ BikeStore/Scripts/AdminCrudFunctions.js
@@ -0,0 +1,22 @@
+﻿
+$(document).ready(function () {
+
+    var ddlBrandName = $('#ddlBrandName');
+
+    var fnOnBrandChange = function () {
+        var selectedBrandID = ddlBrandName.val();
+        
+        if (selectedBrandID == "")
+        {            
+            //if value is blank, give popup to add brand, then append to list
+            var newBrandName = prompt('Enter new brand name', '', '-');
+            $('<option value=-1>' + newBrandName + '</option>').appendTo(ddlBrandName);
+            $('#ddlBrandName option:last-child').attr('selected', 'selected');
+
+        }
+    };
+
+    $('#ddlBrandName').change(fnOnBrandChange);
+
+    //http://andyck1.blogspot.com/2012/06/aspnet-dropdownlist-postback-jquery.html
+});
